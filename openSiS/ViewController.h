6fee086b6c4e9e9494692d58b4826abd796ad2cc
EjobIndia <ejobindia@EjobIndias-Mac-mini.local>
2015-08-10 12:26:17 +0530

    Initial Commit

---
+++ openSiS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  openSiS
+//
+//  Created by EjobIndia on 10/08/15.
+//  Copyright (c) 2015 openSiS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ openSiS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  openSiS
+//
+//  Created by EjobIndia on 10/08/15.
+//  Copyright (c) 2015 openSiS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
