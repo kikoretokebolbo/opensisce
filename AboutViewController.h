94b5e49cf16b7c1ae3a8e465e95b6ec76045403a
EjobIndia <ejobindia@ejobindias-mac-mini.local>
2016-06-23 12:39:39 +0530

    p

---
+++ AboutViewController.h
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.h
+//  openSiS
+//
+//  Created by os4ed on 4/6/16.
+//  Copyright © 2016 openSiS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+{
+
+    NSString *str;
+}
+
+@end
